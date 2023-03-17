b43f808e4384e34d444fae2399bca8e173b946b1
Dmitry Savchenko <dmitrysvch1@gmail.com>
2023-03-17 17:15:55 +0200

    added background when loading

--- weather-app-test/Views/ContentView.swift
+++ weather-app-test/Views/ContentView.swift
@@ -7,57 +7,52 @@
 
 import SwiftUI
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
 
 struct ContentView: View {
     @State private var isNight = false
     
-    @ObservedObject private var viewModel: MainViewModel
-    
-    init() {
-       viewModel = MainViewModel()
-    }
-    
+    @ObservedObject private var viewModel =  MainViewModel()
     
     //MARK: ContentView
     var body: some View {
-        
-        NavigationView {
-           
-                ZStack {
-                    BackgroundView(isNight: $isNight)
-                    ScrollView {
-                    //MARK: Main Stack
-                        VStack {
-                            LocationView(location: (viewModel.currentWeatherDataSource?.location.name ?? "") + ", " + (viewModel.currentWeatherDataSource?.location.country ?? "") , isNight: $isNight)
-                                .padding(.top, 0)
-                            
-                            MainTemperatureView(isNight: $isNight, currentWeather: viewModel.currentWeatherDataSource?.current, weatherImage: isNight ? ImageNames.moonAndStars.rawValue : ImageNames.cloudAndSun.rawValue , date: Float(viewModel.currentWeatherDataSource?.current.tempC ?? 0))
-                                .padding(.bottom, 50)
-                            
-                            Spacer()
-                            
-                            ScrollView(.horizontal) {
-                                HStack(spacing: 30) {
-                                    
-                                    ForEach(0..<(viewModel.currentWeatherDataSource?.forecast.forecastday.count ?? 0), id: \.self) {
+        ZStack {
+            if (viewModel.viewModelStatus == MainViewModelStatus.loading) {
+                BackgroundView(isNight: $isNight)
+            } else {
+                NavigationView {
+                    ZStack {
+                        BackgroundView(isNight: $isNight)
+                        ScrollView {
+                            VStack {
+                                LocationView(location: (viewModel.currentWeatherDataSource?.location.name ?? "") + ", " + (viewModel.currentWeatherDataSource?.location.country ?? "") , isNight: $isNight)
+                                    .padding(.top, 0)
+                                
+                                MainTemperatureView(isNight: $isNight, currentWeather: viewModel.currentWeatherDataSource?.current, weatherImage: isNight ? ImageNames.moonAndStars.rawValue : ImageNames.cloudAndSun.rawValue , date: Float(viewModel.currentWeatherDataSource?.current.tempC ?? 0))
+                                    .padding(.bottom, 50)
+                                
+                                Spacer()
+                                
+                                ScrollView(.horizontal) {
+                                    HStack(spacing: 30) {
                                         
-                                        DailyWeatherView(withForecaset: (viewModel.currentWeatherDataSource?.forecast.forecastday[$0])!)
+                                        ForEach(0..<(viewModel.currentWeatherDataSource?.forecast.forecastday.count ?? 0), id: \.self) {
+                                            
+                                            DailyWeatherView(withForecaset: (viewModel.currentWeatherDataSource?.forecast.forecastday[$0])!)
+                                        }
                                     }
                                 }
+                                Spacer()
                             }
-                            Spacer()
                         }
-                    }.onAppear(perform: {
-                        viewModel.callForecastAPI()
-                    })
-                
-            }.navigationBarHidden(true)
-        }.accentColor(.black)
+                        
+                    }.navigationBarHidden(true)
+                }
+            }
+        }.onAppear(perform: {
+            viewModel.callForecastAPI()
+        })
+        .accentColor(.black)
     }
 }
 
--- weather-app-test/Views/MainViewModel.swift
+++ weather-app-test/Views/MainViewModel.swift
@@ -14,9 +14,6 @@ extension ContentView {
         @Published var currentWeatherDataSource: ForecastResponse?
         @Published var viewModelStatus = MainViewModelStatus.loading;
        
-        init() {
-            callForecastAPI()
-        }
         func callForecastAPI() {
             viewModelStatus = .loading
             let currentCity = UserDefaults.standard.object(forKey: "currentCity") as? String
